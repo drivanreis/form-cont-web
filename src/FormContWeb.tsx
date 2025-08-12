f50ef35aebba53aba4f0ad433838775e6eadd6e9
drivanreis <105787691+drivanreis@users.noreply.github.com>
2025-08-12 02:38:32 -0300

    Iniciando projeto

---
+++ src/FormContWeb.tsx
@@ -0,0 +1,16 @@
+import './FormContWeb.css' 
+
+export default function FormContWeb() {
+  return (
+    <div className="form-cont-web-container">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSdEtqTMKOc4_4bnWIAsDKjKdoOLhpCxHuDc0y-4ZIR3ZZ6xpA/viewform?embedded=true"
+        title="Formulário de Contato Amber"
+        loading="lazy"
+        allowFullScreen
+      >
+        Carregando…
+      </iframe>
+    </div>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+// File: src/main.tsx
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FormContWeb from './FormContWeb'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <FormContWeb />
+  </React.StrictMode>
+)
