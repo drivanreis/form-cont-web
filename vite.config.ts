ab1dc89bf732bea03828ce6d2a77f0e2487cc204
drivanreis <105787691+drivanreis@users.noreply.github.com>
2025-08-12 03:30:33 -0300

    correção vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
+// File: vite.config.ts
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/form-cont-web/', // 👈 nome exato do repositório
 })
